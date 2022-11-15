02552ac55affe5c22e6daa184a50ce8a571eb729
Edouard Klein <edk@beaver-labs.com>
2022-11-15 22:24:59 +0100

    Slides

---
+++ Slides/01intro/Slides.lisp
@@ -0,0 +1,101 @@
+(in-package :cl-smt-lib)
+
+(slideshow
+ (slide
+  (title "Python for business"))
+ (slide
+  (title "What is this course about ?")
+  (bullet "\"lots of rambling into completely unrelated nonsense\""))
+ (slide
+  (title "Culture")
+  (image "arrowhead.jpg"))
+ (slide
+  (image "bowman.jpg"))
+ (slide
+  (title "Culture")
+  (image "lissoir.jpg"))
+ (slide
+  (image "leather.jpg")
+  (url "https://doi.org/10.1073/pnas.1302730110")
+  (url "https://www.nature.com/articles/nature.2013.13542"))
+ (slide
+  (title "Chesterton's fence")
+  (image "fence.jpg"))
+ (slide
+  (title "Cargo culting")
+  (image "cargocult.jpg")
+  (url "https://www.iamag.co/short-movie-cargo-cult/")
+  (url "http://doi.org/10.29164/18cargo"))
+ (slide
+  (title "What is this course about ?")
+  (url "https://google.com")
+  (bullet "Actually a search-engine usage course"))
+ (slide
+  (title "What is this course about ?")
+  (image "anger.jpg")
+  (bullet "Actually an anger management course"))
+ (slide
+  (title "What is this course about")
+  (bullet "Solving real world problems with a computer"))
+ (slide
+  (title "Standard session")
+  (image "standard.png"))
+ (slide
+  (title "Between two sessions")
+  (bullet "Learn the Anki Deck")
+  (url "https://apps.ankiweb.net/")
+  (bullet "Do the assignment")
+  (bullet "Ask questions by email"))
+ (slide
+  (title "Assignments")
+  (bullet "One between two sessions")
+  (bullet "Optional")
+  (bullet "May save you from failing")
+  (bullet "Send, by email, a link to a notebook"))
+ (slide
+  (title "Grading")
+  (bullet "Challenge are graded binarily (except the last one)")
+  (bullet "10 challenges (one per session)")
+  (bullet "7 and above get an A")
+  (bullet "6 == B")
+  (bullet "5 == C")
+  (bullet "Below C, you fail"))
+ (slide
+  (title "Grading")
+  (bullet "The automatic grade is final, no negociating")
+  (bullet "Good assignments, on time == C")
+  (bullet "Assignments are great for training"))
+ (slide
+  (title "Cheating")
+  (bullet "Caught cheating == failed")
+  (bullet "No second chances")
+  (bullet "No excuses"))
+ (slide
+  (title "What is cheating ?")
+  (p "Communicating with another human being during a challenge.")
+  (bullet "Making someone else take the challenge in your stead")
+  (bullet "Hacking the challenge server")
+  (bullet "Anything else is fair game"))
+ (slide
+  (title "Bug reports")
+  (p "Reporting a bug in a challenge is an automatic pass for this challenge")
+  (p "Reporting a security vulnerability in the challenge server is an automatic pass for the course"))
+ (slide
+  (title "Course's page")
+  (url "https://rdklein.fr/courses/python101")
+  (url "https://essec.rdklein.fr"))
+ (slide
+  (title "Python's syntax")
+  (url "https://marko-knoebl.github.io/slides/python-all-en.html"))
+ (slide
+  (title "Python's syntax highlight")
+  (bullet "Expression")
+  (bullet "Statement"))
+ (slide
+  (title "Running Python Code")
+  (url "https://colab.research.google.com")
+  (url "https://jupyter.org/"))
+ (slide
+  (title "Assignment")
+  (url "https://adventofcode.com/")
+  (bullet "Do the first problem of any year")))
---
+++ Slides/06amendment/Slides.lisp
@@ -0,0 +1,11 @@
+(in-package :cl-smt-lib)
+
+(slideshow
+ (slide
+  (title "Je vous ai compris")
+  (image "degaulle.jpg"))
+ (slide
+  (title "Grading")
+  (bullet "First 20 minutes: 1 point")
+  (bullet "Rest of the week: half a point")
+  (bullet "Participation token: one tenth of a point")))
