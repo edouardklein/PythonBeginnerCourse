1e4543dee46f84b0bf5c0620cba2121ff50ef625
Edouard Klein <edk@beaver-labs.com>
2024-02-12 09:02:23 +0100

    WIP on the landing page + graphical assets

--- LandingPage/backend.py
+++ LandingPage/backend.py
@@ -1,13 +1,65 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, render_template
+from datetime import datetime
+from contextlib import contextmanager
+import fcntl
 
 app = Flask(__name__)
 
+@contextmanager
+def locked_file(file_path, mode='a'):
+    """ Context manager for locking a file. """
+    with open(file_path, mode) as file:
+        fcntl.flock(file.fileno(), fcntl.LOCK_EX)
+        try:
+            yield file
+        finally:
+            fcntl.flock(file.fileno(), fcntl.LOCK_UN)
+
 @app.route('/forms/enroll', methods=['POST'])
 def enroll():
-    print("Received data:")
     for key in request.form:
-        print(f"{key}: {request.form[key]}")
+        app.logger.info(f"{key}: {request.form[key]}")
     return "Form data received."
 
+@app.route('/forms/subscribe', methods=['POST'])
+def subscribe():
+    if "subscriber_email" not in request.form:
+        return redirect('/')
+    with locked_file("/var/lib/handsonpython/subscribers.txt", 'a') as f:
+        f.write(request.form['subscriber_email'] + "\n")
+    return redirect("../subscribed.html")
+
+def disable_radio(file_path, booking_date):
+    # Read the content of the HTML file
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    # Modify the corresponding line
+    new_lines = []
+    for line in lines:
+        if booking_date in line:
+            # Add the 'disabled' attribute to the input tag
+            line = line.replace(f'value="{booking_date}"', f'value="{booking_date}" disabled')
+            # Append '[Already booked]' to the label content
+            line = line.replace('</label>', ' [Already booked]</label>')
+        new_lines.append(line)
+
+    # Write the modified content back to the file
+    with open(file_path, 'w') as file:
+        file.writelines(new_lines)
+
+@app.route('/forms/ooo', methods=['POST'])
+def ooo():
+    if "subscriber_email" not in request.form:
+        return redirect('/')
+    if 'date' not in request.form:
+        return redirect("../missingdate.html")
+    with locked_file("/var/lib/handsonpython/ooo.txt", 'a') as f:
+        f.write(f"{request.form['date']}, {request.form['subscriber_email']} \n")
+    human_readable_date = datetime.strptime(
+        request.form['date'],
+        '%Y%m%dT%H:%M').strftime("%B %d, %Y at %H:%M")
+    return render_template('ooo.html', date=human_readable_date)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
--- LandingPage/os.scm
+++ LandingPage/os.scm
@@ -1,9 +1,26 @@
 (use-modules
- (beaver system))
+ (beaver system)
+ (gnu packages python)
+ (gnu packages python-web))
 
 (->
  (minimal-ovh "osef")
  (users "edouard")
+ (groups "edouard")
  (http-static-content #:from-host "osef"
                       #:to-dir "/home/edouard/src/PythonBeginnerCourse/LandingPage"
-                      #:nomkdir #t))
+                      #:nomkdir #t
+                      #:raw-content '("location /forms/ {"
+                                      "proxy_pass http://127.0.0.1:5000;"
+                                      "}"))
+ (os/mkdir-p "/var/lib/handsonpython"
+             #:mode #o755
+             #:owner "edouard"
+             #:group "edouard")
+ (packages python python-flask)
+ (os/shepherd-service
+  ;;  ;;"for i in $(seq 10); do echo $i; sleep 10 ; done"
+  "FLASK_ENV=development FLASK_DEBUG=1 FLASK_APP=/home/edouard/src/PythonBeginnerCourse/LandingPage/backend.py  flask run --reload --debugger --host=0.0.0.0 --port=5000"
+  "/home/edouard/src/PythonBeginnerCourse/LandingPage/")
+  ;;(os/git)
+ )
