84f0dd0ed104a688b24818fe8a483bb21d718821
Edouard Klein <edouard@venerable>
2024-05-17 15:46:45 +0200

    plop

--- LandingPage/os.scm
+++ LandingPage/os.scm
@@ -5,22 +5,17 @@
 
 (->
  (minimal-ovh "osef")
- (users "edouard")
- (groups "edouard")
+ (groups "handsonpython")
+ (nobody-like-user "handsonpython")
  (http-static-content #:from-host "osef"
-                      #:to-dir "/home/edouard/src/PythonBeginnerCourse/LandingPage"
+                      #:to-dir "/var/lib/handsonpython"
                       #:nomkdir #t
                       #:raw-content '("location /forms/ {"
                                       "proxy_pass http://127.0.0.1:5000;"
                                       "}"))
- (os/mkdir-p "/var/lib/handsonpython"
-             #:mode #o755
-             #:owner "edouard"
-             #:group "edouard")
+ ;; os/mkdir-p /var/lib/handsonpython
  (packages python python-flask)
  (os/shepherd-service
-  ;;  ;;"for i in $(seq 10); do echo $i; sleep 10 ; done"
-  "FLASK_ENV=development FLASK_DEBUG=1 FLASK_APP=/home/edouard/src/PythonBeginnerCourse/LandingPage/backend.py  flask run --reload --debugger --host=0.0.0.0 --port=5000"
-  "/home/edouard/src/PythonBeginnerCourse/LandingPage/")
-  ;;(os/git)
+  "FLASK_ENV=development FLASK_DEBUG=1 FLASK_APP=/var/lib/handsonpython/backend.py  flask run --reload --debugger --host=0.0.0.0 --port=5000"
+  "/var/lib/handsonpython")
  )
